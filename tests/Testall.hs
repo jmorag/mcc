99a7a2b545fdc85f1e230617828cdb5e0ba6899a
Joseph Morag <jm4157@columbia.edu>
2018-08-18 13:51:24 -0400

    Add extra dependencies to cabal file

--- tests/Testall.hs
+++ tests/Testall.hs
@@ -4,9 +4,14 @@ import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsFile, findByExtension)
 import System.FilePath (takeBaseName, replaceExtension)
 
-import           Data.Text (Text)
+import Microc
+
+import           Data.String.Conversions
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import           Data.Text (Text)
+
+import System.IO.Silently
 
 -- | Given a microc file, attempt to compile and execute it and write the
 -- results to a new file to be compared with what should be the correct output
@@ -15,9 +20,14 @@ runFile infile outfile = do
   program <- T.readFile infile
   let parseTree = runParser programP (show infile) program
   case parseTree of
-    Left _ -> parseTest' programP program -- write this to the outfile
+    Left _ -> redirect $ parseTest' programP program
     Right ast -> case checkProgram ast of
-      Left err -> return () -- to be completed
+      Left err -> redirect (T.putStrLn err)
+      Right sast -> do
+        let llvmModule = codegenProgram sast
+        redirect (run llvmModule)
+  where
+    redirect action = cs <$> capture_ action >>= T.writeFile outfile
 
 main :: IO ()
 main = defaultMain =<< goldenTests
