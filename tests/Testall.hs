cd67fff12fb0f460d8e35df7e190ddc40652b605
Joseph Morag <jm4157@columbia.edu>
2018-08-18 14:38:58 -0400

    Move tests out of separate directories and finish golden files

--- tests/Testall.hs
+++ tests/Testall.hs
@@ -1,7 +1,7 @@
 module Testall where
 
 import Test.Tasty (defaultMain, TestTree, testGroup)
-import Test.Tasty.Golden (goldenVsFile, findByExtension)
+import Test.Tasty.Golden
 import System.FilePath (takeBaseName, replaceExtension)
 
 import Microc
@@ -11,12 +11,14 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import           Data.Text (Text)
 
+import qualified Data.ByteString.Lazy as LBS
+
 import System.IO.Silently
 
 -- | Given a microc file, attempt to compile and execute it and write the
 -- results to a new file to be compared with what should be the correct output
-runFile :: FilePath -> FilePath -> IO ()
-runFile infile outfile = do
+runFile :: FilePath -> IO LBS.ByteString
+runFile infile = do
   program <- T.readFile infile
   let parseTree = runParser programP (show infile) program
   case parseTree of
@@ -27,7 +29,15 @@ runFile infile outfile = do
         let llvmModule = codegenProgram sast
         redirect (run llvmModule)
   where
-    redirect action = cs <$> capture_ action >>= T.writeFile outfile
+    redirect action = cs <$> capture_ action
 
 main :: IO ()
 main = defaultMain =<< goldenTests
+
+goldenTests :: IO TestTree
+goldenTests = do
+  mcFiles <- findByExtension [".mc"] "."
+  return $ testGroup "microc golden tests"
+    [ goldenVsString (takeBaseName mcFile) outFile (runFile mcFile) 
+      | mcFile <- mcFiles, let outFile = replaceExtension mcFile ".out" ]
+
