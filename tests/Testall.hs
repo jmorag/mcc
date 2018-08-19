c97360845ebf4ed325ce3cf09ac93d7dec2af0fb
Joseph Morag <jm4157@columbia.edu>
2018-08-19 12:21:11 -0400

    Remove bytestring from dependencies and rely on convertString instead

--- tests/Testall.hs
+++ tests/Testall.hs
@@ -8,13 +8,13 @@ import Microc
 
 import           Data.String.Conversions
 import qualified Data.Text.IO as T
-import           Data.ByteString.Lazy (ByteString)
+import           Data.Text (Text)
 
 import System.IO.Silently
 
 -- | Given a microc file, attempt to compile and execute it and write the
 -- results to a new file to be compared with what should be the correct output
-runFile :: FilePath -> IO ByteString
+runFile :: FilePath -> IO Text
 runFile infile = do
   program <- T.readFile infile
   let parseTree = runParser programP (cs infile) program
@@ -31,13 +31,16 @@ runFile infile = do
 main :: IO ()
 main = defaultMain =<< goldenTests
 
+-- | All of the test cases
+-- General structure taken from 
+-- https://ro-che.info/articles/2017-12-04-golden-tests
 goldenTests :: IO TestTree
 goldenTests = do
   passingFiles <- findByExtension [".mc"] "tests/pass"
   failingFiles <- findByExtension [".mc"] "tests/fail"
   return $ testGroup "microc compiler output tests" $
-    [ goldenVsString (takeBaseName mcFile) outFile (runFile mcFile) 
+    [ goldenVsString (takeBaseName mcFile) outFile (cs <$> runFile mcFile) 
       | mcFile <- passingFiles, let outFile = replaceExtension mcFile ".out" ]
       ++
-    [ goldenVsString (takeBaseName mcFile) outFile (runFile mcFile) 
+    [ goldenVsString (takeBaseName mcFile) outFile (cs <$> runFile mcFile) 
       | mcFile <- failingFiles, let outFile = replaceExtension mcFile ".err" ]
