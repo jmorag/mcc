8b7cfd2b363ae7f0efa7ae7a3eb92305c6f241be
Joseph Morag <jm4157@columbia.edu>
2018-08-26 12:52:45 -0400

    Correctly use the Semant monad for binds checking

--- src/Microc/Semant.hs
+++ src/Microc/Semant.hs
@@ -1,36 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
-module Microc.Semant (checkProgram, builtIns) where
+{-# LANGUAGE LambdaCase #-}
+module Microc.Semant (checkProgram) where
 
 import Microc.Ast
 import Microc.Sast
 import qualified Data.Map as M
-import Data.Tuple (swap)
 import Control.Monad.State
 import Control.Monad.Except
-import Data.Maybe (isNothing)
+import Data.Maybe (isJust)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import Data.List (find)
 
-type Vars = M.Map Text Type
+type Vars = M.Map (Text, VarKind) Type
 type Funcs = M.Map Text Function
-data Env = Env { vars :: Vars, funcs :: Funcs, thisFunc :: Function }
+data VarKind = Global | Formal | Local deriving (Show, Eq, Ord)
+
+data Env = Env { vars     :: Vars
+               , funcs    :: Funcs
+               , thisFunc :: Function }
 
 type SemantS = ExceptT Text (State Env)
 
-guardInfo :: MonadError Text m => Bool -> Text -> m a -> m a
+tshow :: Show a => a -> Text
+tshow = T.pack . show
+
+guardInfo :: MonadError e m => Bool -> e -> m a -> m a
 guardInfo cond msg rest = if not cond then throwError msg else rest
 
-checkBinds :: Text -> [Bind] -> [Bind]
-checkBinds kind = go M.empty
-  where
-    go checked [] = map (\(name, typ) -> (typ, name)) (M.toList checked)
-    go checked (b:bs) = case b of 
-      (TyVoid, _) -> error . T.unpack $ "Illegal void binding " <> kind <> " " <> snd b
-      _ -> case M.lookup (snd b) checked of
-        Nothing -> go (M.insert (snd b) (fst b) checked) bs
-        Just _ -> error . T.unpack $ "Illegal duplicate binding " <> kind <> " " <> snd b
+checkBinds :: VarKind -> [Bind] -> SemantS [Bind]
+checkBinds kind = mapM $ \case
+    (TyVoid, name) -> throwError $ 
+      T.unwords ["illegal void binding in", tshow kind, "variable", name]
+    (ty, name) -> do
+      vars <- gets vars
+      guardInfo (M.notMember (name, kind) vars)
+                (T.unwords ["Illegal duplicate", tshow kind, "binding", name]) $ do
+                modify $ \env -> env { vars = M.insert (name, kind) ty vars } 
+                return (ty, name)
 
 builtIns :: Funcs
 builtIns = M.fromList $ map toFunc
@@ -47,9 +53,10 @@ checkExpr expr = let isNumeric t = t `elem` [TyInt, TyFloat] in case expr of
 
   Id s -> do
     vars <- gets vars
-    case M.lookup s vars of
-      Just ty -> return (ty, SId s)
+    let foundVars = map (\kind -> M.lookup (s, kind) vars) [Local, Formal, Global]
+    case join $ find isJust foundVars of
       Nothing -> throwError $ "Unbound variable " <> s
+      Just ty -> return (ty, SId s)
 
   Binop op lhs rhs -> do
     lhs'@(t1, _) <- checkExpr lhs
@@ -80,14 +87,12 @@ checkExpr expr = let isNumeric t = t `elem` [TyInt, TyFloat] in case expr of
              return (ty, SUnop Not e')
 
   Assign s e -> do
-    e'@(ty, _) <- checkExpr e
-    vars <- gets vars
-    case M.lookup s vars of
-      Nothing -> throwError $ "Unbound variable " <> s
-      Just ty' -> guardInfo (ty == ty') 
-                  "Attempt to assign expression to var of incompatible type" $
-                  return (ty, SAssign s e')
-
+    rhs@(ty, _) <- checkExpr e
+    (ty', _) <- checkExpr (Id s)
+    guardInfo (ty == ty') 
+     "Attempt to assign expression to var of incompatible type" $
+     return (ty, SAssign s rhs)
+    
   Call s es -> do
     funcs <- gets funcs
     case M.lookup s funcs of
@@ -119,9 +124,8 @@ checkStatement stmt = case stmt of
 
   While cond action -> do
     cond'@(ty, _) <- checkExpr cond
-    guardInfo (ty == TyBool) "Expected boolean expression" $ do
-      action' <- checkStatement action
-      return $ SWhile cond' action'
+    guardInfo (ty == TyBool) "Expected boolean expression" $
+      SWhile cond' <$> checkStatement action
 
   Return expr -> do
     e@(ty, _) <- checkExpr expr
@@ -133,7 +137,7 @@ checkStatement stmt = case stmt of
   Block sl -> 
     let flattened = flatten sl
     in guardInfo (nothingFollowsRet flattened)
-       ("Nothing can follow a return: error in " <> T.pack (show stmt)) $
+       ("Nothing can follow a return: error in " <> tshow stmt) $
        SBlock <$> mapM checkStatement sl
     where
       flatten [] = []
@@ -145,55 +149,47 @@ checkStatement stmt = case stmt of
       nothingFollowsRet (s:ss) = 
         case s of Return _ -> False; _ -> nothingFollowsRet ss
 
-  
-    --case sl of
-    ---- unsure is this first case is necessary...
-    --[s@(Return _)] -> do s' <- checkStatement s; return $ SBlock [s']
-    ---------------------------
-    --Return _ : _ -> throwError $ "nothing can follow a return: error in " <> T.pack (show stmt)
-    --Block sl : ss -> checkStatement $ Block (sl ++ ss)
-    --_ -> SBlock <$> mapM checkStatement sl
-
-    
 checkFunction :: Function -> SemantS SFunction    
 checkFunction func = do
   -- add the fname to the table and check for conflicts
   funcs <- gets funcs
-  guardInfo (isNothing $ M.lookup (name func) funcs) 
+  guardInfo (M.notMember (name func) funcs) 
             ("Redeclaration of function " <> name func) $
     -- add this func to symbol table
     modify $ \env -> 
       env { funcs = M.insert (name func) func funcs, thisFunc = func }
 
+  -- Save the symbol table prior to adding formal and local variables to it
+  oldState <- get
+
   -- check variables
-  let formals' = checkBinds "formal" (formals func)
-  let locals'  = checkBinds "local"  (locals func)
-  -- create local variable table
-  globals <- gets vars
-  let allVars = M.toList globals ++ map swap (formals' ++ locals')
-      localVars = M.fromList allVars
+  formals' <- checkBinds Formal (formals func)
+  locals'  <- checkBinds Local  (locals func)
 
-  -- Overwrite local variables into the environment for the body checking
-  modify $ \env -> env { vars = localVars }
-  body' <- mapM checkStatement (body func)
-  -- Set the env back to the way it was
-  modify $ \env -> env { vars = globals }
+  -- Check the body of the function with all the local variables added to the
+  -- symbol table
+  body' <- checkStatement (Block $ body func)
 
-  return $ SFunction { styp = typ func 
-                     , sname = name func
-                     , sformals = formals'
-                     , slocals = locals'
-                     , sbody = body'
-                     }
+  -- remove all local variables from symbol table
+  put oldState
+
+  case body' of 
+    SBlock body'' -> return $ SFunction { styp = typ func 
+                                        , sname = name func
+                                        , sformals = formals'
+                                        , slocals = locals'
+                                        , sbody = body''
+                                        }
+    _ -> error "Internal error - block didn't become a block?"
 
 
 checkProgram :: Program -> Either Text SProgram
 checkProgram (binds, funcs) = 
   evalState (runExceptT (checkProgram' (binds, funcs))) baseEnv
   where
-  baseEnv = Env { vars = M.empty, 
-                  funcs = builtIns, 
-                  thisFunc = garbageFunc }
+  baseEnv = Env { vars = M.empty
+                , funcs = builtIns
+                , thisFunc = garbageFunc }
   garbageFunc = Function { typ = TyVoid
                          , name = ""
                          , formals = []
@@ -201,10 +197,8 @@ checkProgram (binds, funcs) =
                          , body = []
                          }
   checkProgram' (binds, funcs) = do
-    let globals = checkBinds "global" binds
-    modify $ \env -> env { vars = M.fromList (map swap globals) }
+    globals <- checkBinds Global binds
     funcs' <- mapM checkFunction funcs
     case find (\f -> sname f == "main") funcs' of
       Nothing -> throwError "Error, main function not defined"
       Just _ -> return (globals, funcs')
-  
