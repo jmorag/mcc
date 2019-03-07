50c8294161831d0010479e70740446f291b69332
Joseph Morag <jm4157@columbia.edu>
2019-03-07 13:58:31 -0500

    Tests now compile

--- tests/Testall.hs
+++ tests/Testall.hs
@@ -13,20 +13,17 @@ import           Microc
 
 import           Data.String.Conversions
 import qualified Data.Text.IO                  as T
-import           Data.Text                      ( Text )
+import           Data.Text                      ( Text, unpack )
 import           Data.Text.Prettyprint.Doc
 import           Data.Text.Prettyprint.Doc.Render.Text
-import           Text.Megaparsec                ( errorBundlePretty )
 
 -- | Given a microc file, attempt to compile and execute it and read the
 -- results to be compared with what should be the correct output
 runFile :: FilePath -> IO Text
 runFile infile = do
   program <- T.readFile infile
-  let parseTree = runParser programP (cs infile) program
-  case parseTree of
-    Left  e   -> return . cs $ errorBundlePretty e
-    Right ast -> case checkProgram ast of
+  let ast = parse . alexScanTokens $ unpack program
+  case checkProgram ast of
       Left err ->
         return . renderStrict $ layoutPretty defaultLayoutOptions (pretty err)
       Right sast -> run (codegenProgram sast)
