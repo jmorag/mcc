1079b532ec2a5572460cc45ce5f3817b8bde7f2c
Joseph Morag <jm4157@columbia.edu>
2019-04-12 00:11:49 -0400

    Use parser generator in default tests

--- tests/Testall.hs
+++ tests/Testall.hs
@@ -27,11 +27,13 @@ import           Control.Exception
 runFile :: FilePath -> IO Text
 runFile infile = do
   program <- T.readFile infile
-  let ast = parse . alexScanTokens $ unpack program
-  case checkProgram ast of
-    Left err ->
-      return . renderStrict $ layoutPretty defaultLayoutOptions (pretty err)
-    Right sast -> run (codegenProgram sast)
+  let parseTree = runParser programP (cs infile) program
+  case parseTree of
+    Left  e   -> return . cs $ errorBundlePretty e
+    Right ast -> case checkProgram ast of
+      Left err ->
+        return . renderStrict $ layoutPretty defaultLayoutOptions (pretty err)
+      Right sast -> run (codegenProgram sast)
 
 main :: IO ()
 main = defaultMain =<< goldenTests
