e627ef22e11a71a9d6d7e3e3f2e150896f890e53
Joseph Morag <jm4157@columbia.edu>
2020-03-18 13:51:15 -0400

    Bump versions and add default.nix

---
+++ default.nix
@@ -0,0 +1,35 @@
+let
+  compilerVersion = "ghc883";
+  config = {
+    packageOverrides = pkgs: rec {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          ghc883 = pkgs.haskell.packages."${compilerVersion}".override {
+            overrides = self: super: {
+              llvm-hs-pretty = pkgs.haskell.lib.dontCheck super.llvm-hs-pretty;
+            };
+          };
+        };
+      };
+    };
+    allowBroken = true;
+  };
+  pkgs = import (builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-unstable-2020-03-17";
+    url =
+      "https://github.com/nixos/nixpkgs/archive/a2e06fc3423c4be53181b15c28dfbe0bcf67dd73.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "0bjx4iq6nyhj47q5zkqsbfgng445xwprrslj1xrv56142jn8n5r9";
+  }) { inherit config; };
+  compiler = pkgs.haskell.packages."${compilerVersion}";
+  pkg = compiler.developPackage {
+    root = ./.;
+    source-overrides = { };
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv
+      (with pkgs.haskellPackages; [ cabal-install alex happy ]);
+  };
+  buildInputs = [ pkgs.llvm_9 pkgs.clang_9 ];
+in pkg.overrideAttrs
+(attrs: { buildInputs = attrs.buildInputs ++ buildInputs; })
