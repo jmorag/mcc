d412eec5e3783873ad42b74b837ed0f1047c2fde
Joseph Morag <jm4157@columbia.edu>
2020-05-13 12:33:24 -0400

    Improve default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 let
+  inherit (import <nixpkgs> { }) fetchFromGitHub;
+  nixpkgs = fetchFromGitHub {
+    name = "nixos-unstable-2020-03-17";
+    owner = "nixos";
+    repo = "nixpkgs";
+    rev = "a2e06fc3423c4be53181b15c28dfbe0bcf67dd73";
+    sha256 = "0bjx4iq6nyhj47q5zkqsbfgng445xwprrslj1xrv56142jn8n5r9";
+  };
+  pkgs = import nixpkgs { inherit config; };
   compilerVersion = "ghc883";
   config = {
     packageOverrides = pkgs: rec {
@@ -11,9 +20,10 @@ let
               overrides = self: super: {
                 llvm-hs-pretty =
                   pkgs.haskell.lib.dontCheck super.llvm-hs-pretty;
-                llvm-hs = super.callHackage "llvm-hs" "9.0.1" {
-                  llvm-config = pkgs.llvm_9;
-                };
+                llvm-hs = pkgs.haskell.lib.dontCheck
+                  (super.callHackage "llvm-hs" "9.0.1" {
+                    llvm-config = llvm_9;
+                  });
               };
             };
         };
@@ -21,22 +31,12 @@ let
     };
     allowBroken = true;
   };
-  pkgs = import (builtins.fetchTarball {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-unstable-2020-03-17";
-    url =
-      "https://github.com/nixos/nixpkgs/archive/a2e06fc3423c4be53181b15c28dfbe0bcf67dd73.tar.gz";
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0bjx4iq6nyhj47q5zkqsbfgng445xwprrslj1xrv56142jn8n5r9";
-  }) { inherit config; };
   compiler = pkgs.haskell.packages."${compilerVersion}";
   pkg = compiler.developPackage {
     root = ./.;
     source-overrides = { };
     modifier = drv:
       pkgs.haskell.lib.addBuildTools drv
-      (with pkgs.haskellPackages; [ cabal-install alex happy ]);
+      (with pkgs.haskellPackages; [ cabal-install alex happy pkgs.clang_9 ]);
   };
-  buildInputs = [ pkgs.llvm_9 pkgs.clang_9 ];
-in pkg.overrideAttrs
-(attrs: { buildInputs = attrs.buildInputs ++ buildInputs; })
+in pkg
